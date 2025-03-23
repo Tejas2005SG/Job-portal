123c506d4e55925cb60c69724f04abf54e4bc24e
Tejas2005SG <tbhangale9@gmail.com>
2025-03-23 12:17:43 +0530

    backend frontend

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,14 @@ app.use("/api/v1/application", applicationRoute);
 
 
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);
